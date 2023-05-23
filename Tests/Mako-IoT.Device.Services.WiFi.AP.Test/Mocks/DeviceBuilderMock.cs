86a6c872aa05fd82a9062b8bb5a9be1625a1d81b
Marcin Torba <torbamarcin@gmail.com>
2023-05-23 19:12:49 +0200

    Update to latest libs. (#5)

--- Tests/Mako-IoT.Device.Services.WiFi.AP.Test/Mocks/DeviceBuilderMock.cs
+++ Tests/Mako-IoT.Device.Services.WiFi.AP.Test/Mocks/DeviceBuilderMock.cs
@@ -18,6 +18,11 @@ namespace Mako_IoT.Device.Services.WiFi.AP.Test.Mocks
             Services = new ServiceCollection();
         }
 
+        public IDeviceBuilder ConfigureDI(ConfigureDIDelegate configureDiAction)
+        {
+            throw new NotImplementedException();
+        }
+
         public IDevice Build()
         {
             throw new NotImplementedException();
