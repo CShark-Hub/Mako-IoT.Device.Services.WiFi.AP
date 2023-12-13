ab22d57e41454c5fb66f6de7e5703a7889cfa524
Marcin Torba <torbamarcin@gmail.com>
2023-12-13 18:04:07 +0100

    Fix tests.

--- Tests/Mako-IoT.Device.Services.WiFi.AP.Test/Mocks/LoggerMock.cs
+++ Tests/Mako-IoT.Device.Services.WiFi.AP.Test/Mocks/LoggerMock.cs
@@ -1,17 +1,107 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using MakoIoT.Device.Services.Interface;
 using System;
 using System.Reflection;
 
 namespace Mako_IoT.Device.Services.WiFi.AP.Test.Mocks
 {
-    internal class LoggerMock : ILogger
+    internal class LoggerMock : ILog
     {
-        public bool IsEnabled(LogLevel logLevel)
+        public void Critical(Exception exception, string message, MethodInfo format)
         {
             throw new NotImplementedException();
         }
 
-        public void Log(LogLevel logLevel, EventId eventId, string state, Exception exception, MethodInfo format)
+        public void Critical(Exception exception, string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Critical(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Critical(Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Error(Exception exception, string message, MethodInfo format)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Error(string message, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Error(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Error(Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Information(Exception exception, string message, MethodInfo format)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Information(Exception exception, string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Information(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Information(Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Trace(Exception exception, string message, MethodInfo format)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Trace(Exception exception, string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Trace(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Trace(Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Warning(Exception exception, string message, MethodInfo format)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Warning(Exception exception, string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Warning(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Warning(Exception exception)
         {
             throw new NotImplementedException();
         }
